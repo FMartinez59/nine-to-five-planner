048746d801004410e13d3d2dbbac2a0ecf56d58e
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-04-05 18:32:28 -0400

    added color change func + save button works, need to retrive text from local data

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,40 @@
-var container = document.getElementById('container');
-
-var hours = [
-  moment().format("9"),
-  moment().format("10"),
-  moment().format("11"),
-  moment().format("12"),
-  moment().format("13"),
-  moment().format("14"),
-  moment().format("15"),
-  moment().format("16"),
-  moment().format("17"),
-]
-
+//displays current day
 var currentDay = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentDay);
 
+$(document).ready(function (){
+  //save button click listener
+  $(".saveBtn").on("click", function () {
+    //gets value from description in JQuery
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-hours.forEach(HOUR=> {
-    var row = document.createElement("div");
-    row.classList.add("row", "time-block");
-    row.innerHTML = `
-    <div id="${hours} class="col-md-1 hour">${hours}</div>
-    <textarea class="col-md-10 description" name="" id=""><textarea>
-    <button class="col-md-1 btn saveBtn">
-        <i class="fas fa-save"></i>
-      </button>
-    </div>`;
-    container.appendChild(row);
-});
+    //stores users input in local storage 
+    localStorage.setItem(time, text);
+  })
+
+  //this is to change the color of the columns based on whether they are in the past,present, or future
+function columColorChanger() {
+  //current time in hours
+  var currentTime = moment().hour();
+//loops over all my time blocks
+  $(".time-block").each(function () {
+    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (timeBlock < currentTime) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (timeBlock === currentTime) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  })
+}
+columColorChanger();
+})
